b5dc73ee0c0f625aea5e80c618f163d044752417
Biswa17 <bswbrh3@gmail.com>
2025-02-16 13:42:57 +0530

    added to admin too

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Booking, Tracking,Document
+
+admin.site.register(Booking)
+admin.site.register(Tracking)
+admin.site.register(Document)
--- cargo/admin.py
+++ cargo/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Cargo, Container
+
+admin.site.register(Cargo)
+admin.site.register(Container)
